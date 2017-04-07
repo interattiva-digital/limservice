64129143e354b50250516c1acf4c2e25653eab94
Margulan Baimbet <thecigaroman@gmail.com>
2017-04-07 20:01:03 +0600

    Nav menu

--- public/theme/limservice/src/js/nav-menu.js
+++ public/theme/limservice/src/js/nav-menu.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
     $('.header__nav').on('click', (e) => {
+        $(e.currentTarget).toggleClass('active');
         $('.nav-menu').toggleClass('active');
         $('.header__contacts-wrap').toggleClass('invisible');
+        $('.header__logo-wrap').toggleClass('active');
     });
 
     $('.header__contacts').on('click', (e) => {
+        $(e.currentTarget).toggleClass('active');
         $('.contacts-menu').toggleClass('active');
         $('.header__nav-wrap').toggleClass('invisible');
+        $('.header__logo-wrap').toggleClass('active');
     });
 });
