e0a7bb04255733565d585511a0c35a9adbc4bf09
Margulan Baimbet <thecigaroman@gmail.com>
2017-04-12 19:02:21 +0600

    About page contacts block

--- public/theme/limservice/src/js/bootstrap.js
+++ public/theme/limservice/src/js/bootstrap.js
@@ -8,3 +8,4 @@ window.Vue = require('vue');
 
 Vue.component('slider', require('./components/slider.vue'));
 Vue.component('slide', require('./components/slide.vue'));
+Vue.component('lim-map', require('./components/map.vue'));
