fd4090037028a12e06a39fdf386633a028b6265f
Margulan Baimbet <thecigaroman@gmail.com>
2017-04-16 21:43:41 +0600

    Landing page slider block

--- public/theme/limservice/src/js/app.js
+++ public/theme/limservice/src/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap.js');
 require('./nav-menu.js');
 require('./slider.js');
 require('./products.js');
+require('./landing.js');
 
 // let app = new Vue({
 //     el: '#app'
---
+++ public/theme/limservice/src/js/landing.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('[data-landing-slider]').slick({
+        prevArrow: $('.slider__prev'),
+        nextArrow: $('.slider__next'),
+    });
+});
