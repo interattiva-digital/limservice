8c624c6e6511078a3ac6c7521c9585bf10987e21
Margulan Baimbet <thecigaroman@gmail.com>
2017-04-25 17:41:55 +0600

    Add services slider mobile styles

--- public/theme/limservice/src/js/app.js
+++ public/theme/limservice/src/js/app.js
@@ -3,6 +3,7 @@ require('./nav-menu.js');
 require('./slider.js');
 require('./products.js');
 require('./landing.js');
+require('./services.js');
 require('./smooth-scrolling.js');
 
 $(document).ready(() => {
---
+++ public/theme/limservice/src/js/services.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('.pros__slider-mobile').slick({
+        arrows: false,
+        dots: true
+    });
+});
