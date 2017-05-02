860563c301c33730eda1dc24fc421ee7c0d4691a
Margulan Baimbet <thecigaroman@gmail.com>
2017-05-02 18:08:50 +0600

    Landing page mobile styles

--- public/theme/limservice/src/js/landing.js
+++ public/theme/limservice/src/js/landing.js
@@ -2,5 +2,15 @@ $(document).ready(() => {
     $('[data-landing-slider]').slick({
         prevArrow: $('.slider__prev'),
         nextArrow: $('.slider__next'),
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    dots: true,
+                    prevArrow: false,
+                    nextArrow: false
+                }
+            }
+        ]
     });
 });
